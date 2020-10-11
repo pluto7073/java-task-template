5620394a76b8295256c3e4953296768aac9732fc
pluto7073 <20planetpluto05@gmail.com>
2020-10-11 14:43:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.beans.*;
+import java.util.Random;
+
+public class Main extends JPanel
+        implements ActionListener,
+        PropertyChangeListener {
+
+    private JProgressBar progressBar;
+    private JButton startButton;
+    private JTextArea taskOutput;
+    private Task task;
+
+    class Task extends SwingWorker<Void, Void> {
+
+        @Override
+        public Void doInBackground() {
+
+            return null;
+        }
+
+        @Override
+        public void done() {
+            Toolkit.getDefaultToolkit().beep();
+            startButton.setEnabled(true);
+            setCursor(null);
+            taskOutput.append("Done!\n");
+        }
+    }
+
+    public Main() {
+        super(new BorderLayout());
+
+        startButton = new JButton("Start");
+        startButton.setActionCommand("start");
+        startButton.addActionListener(this);
+
+        progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+
+        taskOutput = new JTextArea(5, 20);
+        taskOutput.setMargin(new Insets(5,5,5,5));
+        taskOutput.setEditable(false);
+
+        JPanel panel = new JPanel();
+        panel.add(startButton);
+        panel.add(progressBar);
+
+        add(panel, BorderLayout.PAGE_START);
+        add(new JScrollPane(taskOutput), BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        startButton.setEnabled(false);
+        setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        task = new Task();
+        task.addPropertyChangeListener(this);
+        task.execute();
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress" == evt.getPropertyName()) {
+            int progress = (Integer) evt.getNewValue();
+            progressBar.setValue(progress);
+            taskOutput.append(String.format(
+                    "Completed %d%% of task.\n", task.getProgress()));
+        }
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Task Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComponent newContentPane = new Main();
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
